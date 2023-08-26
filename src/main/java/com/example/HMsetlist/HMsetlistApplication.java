74fe98c1d178065d5ba1c93385039a56dd76ffb5
MurkhonovVally <murkhonovbatoha@gmail.com>
2023-08-27 01:14:00 +0300

    Q

---
+++ src/main/java/com/example/HMsetlist/HMsetlistApplication.java
@@ -0,0 +1,13 @@
+package com.example.HMsetlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HMsetlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HMsetlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HMsetlist/HMsetlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HMsetlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HMsetlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
